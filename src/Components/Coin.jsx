fc185ad856b309dc9a2909708b226deb2623b30b
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-01-01 23:49:01 +0100

    Update

--- src/Components/Coin.jsx
+++ src/Components/Coin.jsx
@@ -49,59 +49,53 @@ const Coin = () => {
 
   if (coinData && coinsChart) {
     return (
-      <section className="bg-gradient-to-t from-slate-800 via-slate-700 to-slate-500 min-h-screen pt-10">
-        <div className="flex flex-col gap-5 justify-center items-center">
+      <section className="bg-gradient-to-t from-slate-800 via-slate-700 to-slate-500 min-h-screen pt-10 px-5">
+        <div className="flex flex-col gap-5 justify-center items-center my-[20px] mx-auto mb-5">
           <img
             src={coinData.image.large}
             alt="coin-image"
-            className="h-20 mb-3 rounded-full"
+            className="h-20 max-w-[100px] mb-3 rounded-full"
           />
-          <p className="text-gray-300 font-bold mb-4 text-4xl">
+          <p className="text-gray-300 font-semibold mb-4 md:text-4xl">
             {coinData.name} ({coinData.symbol.toUpperCase()})
           </p>
+        </div>
+        <div className="max-w-[600px] h-[250px] m-auto mb-5">
+          <CoinChart coinsChart={coinsChart} />
+        </div>
 
-          <div className="max-w-[600px] h-250px mx-auto ">
-            <CoinChart coinsChart={coinsChart} />
-          </div>
+        {/* COIN DETAILS */}
+        <div className=" flex flex-col max-w-[600px] text-gray-300 text-2xl">
+          <ul className="flex justify-between border-b-gray-200 pt-3">
+            <li>Coin Market Rank: </li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
 
-          {/* COIN DETAILS */}
+          <ul className="flex">
+            <li>Current Price: </li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.current_price[
+                currency.name
+              ].toLocaleString()}
+            </li>
+          </ul>
 
-          <div className="max-w-[800px] mt-8 text-gray-300 text-2xl py-4 px-2">
-            <ul className="flex">
-              <li>Coin Market Rank: </li>
-              <li>{coinData.market_cap_rank}</li>
-            </ul>
+          <ul className="flex">
+            <li>Coin Market Cap: </li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.market_cap[currency.name].toLocaleString()}
+            </li>
+          </ul>
 
-            <ul className="flex">
-              <li>Current Price: </li>
-              <li>
-                {currency.symbol}
-                {coinData.market_data.current_price[
-                  currency.name
-                ].toLocaleString()}
-              </li>
-            </ul>
-
-            <ul className="flex">
-              <li>Coin Market Cap: </li>
-              <li>
-                {currency.symbol}
-                {coinData.market_data.market_cap[
-                  currency.name
-                ].toLocaleString()}
-              </li>
-            </ul>
-
-            <ul className="flex">
-              <li>ATH: </li>
-              <li>
-                {currency.symbol}
-                {coinData.market_data.market_cap[
-                  currency.name
-                ].toLocaleString()}
-              </li>
-            </ul>
-          </div>
+          <ul className="flex">
+            <li>ATH: </li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.market_cap[currency.name].toLocaleString()}
+            </li>
+          </ul>
         </div>
       </section>
     );
