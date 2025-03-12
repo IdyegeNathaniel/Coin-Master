49ce76696f78758ef27a4464cb3e0508f9d9db33
Idyege Nathaniel <idyegenathaniel@gmail.com>
2025-03-12 02:06:36 +0100

    added responsiveness

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -7,11 +7,16 @@ const WelcomePage = () => {
     const navigate = useNavigate();
 
     return (
-    <section className='w-full h-screen flex flex-col gap-3 justify-center items-center bg-slate-500 text-white'>
-        <h2 className='text-5xl font-bold leading-tight'>Welcome to CoinMaster</h2>
-        <p className='text-gray-300'>Enjoy smooth and fast information on real-time at your finger tips.</p>
-        <Icon />
-        <button onClick={() => {navigate("/home")}} className='flex items-center py-2 px-4 mt-2 bg-white text-slate-950 text-xl rounded hover:bg-slate-950 hover:text-white transition-all duration-300'>Get Going <FaArrowRight className='ml-2' /></button>
+    <section className='w-full h-screen flex flex-col justify-center items-center bg-slate-500 text-white'>
+        <div className="container flex flex-col gap-8 justify-center items-center mx-auto p-4">
+            <div className="text-center">
+                <h2 className='text-3xl md:text-5xl font-bold leading-tight'>Welcome to CoinMaster</h2>
+                <p className='text-gray-300 text-xs'>Enjoy smooth and fast information on real-time at your finger tips.</p>
+            </div>
+            <Icon />
+            <button onClick={() => {navigate("/home")}} className='flex items-center py-2 px-4 mt-2 bg-white text-slate-950 text-xl rounded hover:bg-slate-950 hover:text-white transition-all duration-300'>Get Going <FaArrowRight className='ml-2' /></button>
+    
+        </div>    
     </section>
   )
 }
